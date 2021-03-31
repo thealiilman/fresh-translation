17f527d820615b85fd32a02d3b73067a7c21c73b
thealiilman <aliilman532@gmail.com>
2021-03-31 23:11:02 +0800

    Add German and Irish as available locales

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module FreshTranslation
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.i18n.available_locales = %i[de en ie]
   end
 end
